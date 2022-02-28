e48cbe649169806b00c705bdc8f4f8910ee02428
Kevin Caruso <Kjcaruso12@gmail.com>
2022-02-28 15:44:50 -0600

    Added the ability to see how many animals each employee cares for

--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -28,6 +28,12 @@ export default ({ employee }) => {
         }
     }, [resource])
 
+    useEffect(() => {
+        if (resource?.animals?.length > 0) {
+            setCount(resource.animals.length)
+        }
+    }, [resource])
+
     return (
         <article className={classes}>
             <section className="card-body">
@@ -50,7 +56,7 @@ export default ({ employee }) => {
                     employeeId
                         ? <>
                             <section>
-                                Caring for 0 animals
+                                Caring for {animalCount} animals
                             </section>
                             <section>
                                 Working at unknown location

725d15d7f0137706b7829b1f8942f05a0a11eef3
Van Nguyen <thanh_xer@yahoo.com>
2022-03-02 10:04:13 -0600

    showed animal count and employees count for locations

--- src/components/locations/Location.js
+++ src/components/locations/Location.js
@@ -1,31 +1,74 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+import { Link, useParams } from "react-router-dom"
 import locationImage from "./location.png"
 import "./Location.css"
+import LocationRepository from "../../repositories/LocationRepository";
+// import EmployeeRepository from "../../repositories/EmployeeRepository";
+import useResourceResolver from "../../hooks/resource/useResourceResolver";
 
 //creates jsx of location cards
-
 export default ({location}) => {
+    const [classes, defineClasses] = useState("card location")
+    const [animalCount, updateAnimalCount] = useState(0)
+    const [employeeCount, updateEmployeeCount] = useState(0)
+    const { locationId } = useParams()
+    const { resolveResource, resource } = useResourceResolver()
+    // const [employees, setEmployees] = 
+
+
+    useEffect(() => {
+        if (locationId) {
+            defineClasses("card location--single")
+        }
+        resolveResource(location, locationId, LocationRepository.get)
+    }, [])
+
+    useEffect(() => {
+        if (resource?.animals?.length > 0) {
+            updateAnimalCount(resource.animals.length)
+        }
+    }, [resource])
+
+    useEffect(() => {
+        if (resource?.employeeLocations?.length > 0) {
+            updateEmployeeCount(resource.employeeLocations.length)
+        }
+    }, [resource])
+
     return (
-        <article className="card location" style={{ width: `18rem` }}>
+        <article className={classes} style={{ width: `18rem` }}>
             <section className="card-body">
                 <img alt="Kennel location icon" src={locationImage} className="icon--location" />
                 <h5 className="card-title">
-                    <Link className="card-link"
-                        to={{
-                            pathname: `/locations/${location.id}`,
-                            state: { location: location }
-                        }}>
-                        {location.name}
-                    </Link>
+                    {
+                        locationId
+                            ? resource.name
+                            : <>
+                            <Link className="card-link"
+                                to={{
+                                    pathname: `/locations/${resource.id}`,
+                                    state: { location: resource }
+                                }}>
+                                {resource.name}
+                            </Link>
+                            <section>
+                                Total animal {animalCount}
+                            </section>
+                            <section>
+                                Total Employee {employeeCount}
+                            </section>
+                        </>
+                    }
                 </h5>
+                {/* {
+                    locationId
+                        ? <>
+                            
+                        </>
+                        : ""
+                } */}
             </section>
-            <section>
-                {/* Total animals total animals function */}
-            </section>
-            <section>
-                Total locations 
-            </section>
+            
         </article>
     )
 }
